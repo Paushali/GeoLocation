8ad5c0a051600e11a460631e19a318c1b6652acb
Paushali Kundu <paushalikundu@Paushalis-MacBook-Air.local>
2019-11-13 21:26:23 -0600

    overwrite field

--- GeoLocation.py
+++ GeoLocation.py
@@ -17,7 +17,7 @@ def login():
     if request.method == "GET":
 	return render_template('login.html')
     if request.method == "POST":
-        uname=request.form['username']
+        # some test
         pwd=request.form['pwd']
 	
 	userfile=os.path.dirname(__file__)+'/UserAccessList.txt'
